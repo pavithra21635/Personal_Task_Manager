13a65b7ce20dbc21535756f500571a1b982de240
pavithra21635 <pavithra21635@gmail.com>
2025-04-10 14:36:49 +0530

    Initial commit

---
+++ Personal Task Manager/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Personal_Task_Manager.Models;
+
+namespace Personal_Task_Manager.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            var tasks = new List<TaskModel>
+        {
+            new TaskModel { Title = "IOS App design", DueDate = DateTime.Parse("2025-05-02"), Priority = "High", IsCompleted = true },
+            new TaskModel { Title = "Final Document", DueDate = DateTime.Parse("2025-05-03"), Priority = "High", IsCompleted = false },
+            new TaskModel { Title = "IPC CW report", DueDate = DateTime.Parse("2025-05-03"), Priority = "Medium", IsCompleted = false },
+            new TaskModel { Title = "Agile CW report", DueDate = DateTime.Parse("2025-05-03"), Priority = "Medium", IsCompleted = false },
+        };
+
+
+            return View(tasks);
+        }
+    }
+}
---
+++ Personal Task Manager/Models/TaskModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Personal_Task_Manager.Models
+{
+    public class TaskModel
+    {
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Priority { get; set; } // High / Medium
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
