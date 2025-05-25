856e876511b485ffd912d6eb28acb063b169ee7c
pavithra21635 <pavithra21635@gmail.com>
2025-05-25 19:17:46 +0530

    View tasks development

--- Personal Task Manager/Models/TaskModel.cs
+++ Personal Task Manager/Models/TaskModel.cs
@@ -4,14 +4,32 @@ namespace Personal_Task_Manager.Models
 {
     public class TaskModel
     {
+
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
-        public DateTime StartDate { get; set; }           // ✅ Needed for form
+
+        [Required(ErrorMessage = "Start Date is required")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "Due Date is required")]
         public DateTime DueDate { get; set; }
-        public string Description { get; set; }           // ✅ Needed for form
-        public string Priority { get; set; }              // "High", "Medium", "Low"
+
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Priority is required")]
+        public string Priority { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
         public bool IsCompleted { get; set; }
+
         public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+
     }
 }
